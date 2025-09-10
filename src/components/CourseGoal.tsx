4d305630d0ed8bcb76ecd26daa40ffcaa8d906c3
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-09-11 00:04:41 +0300

    starting

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,14 @@
+const CourseGoal = (props: string) => {
+  return (
+    <article>
+      <div>
+        <h2>TITLE</h2>
+        <p>DESCRIPTION</p>
+      </div>
+      <button>Delete</button>
+      <button>Edit</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
