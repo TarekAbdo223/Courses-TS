4331c26bc78265fc8ae64cc911ddbd80c6b40fd2
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-09-13 11:16:08 +0300

    added the union type checks

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,13 +1,29 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CGoal } from "../App";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps = {
   goals: CGoal[];
   onDelete: (id: string) => void;
 };
 const CourseGoalList = ({ goals, onDelete }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some
+      </InfoBox>
+    );
+  }
+
+  let war;
+
   return (
-    <div>
+    <>
+      {goals.length >= 4 && (
+        <InfoBox mode="warning">
+          You're collecting a lot of goals. Don't put too much on your plate !
+        </InfoBox>
+      )}
       <ul>
         {goals.map((goal) => (
           <li key={goal.id}>
@@ -17,7 +33,7 @@ const CourseGoalList = ({ goals, onDelete }: CourseGoalListProps) => {
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
